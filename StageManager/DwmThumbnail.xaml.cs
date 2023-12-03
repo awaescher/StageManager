78f46262a281cc4d285ef464f568e907c107c660
awaescher <awaescher@gmx.de>
2023-12-03 13:28:42 +0100

    Improve dwm thumbnail arrangement

--- StageManager/DwmThumbnail.xaml.cs
+++ StageManager/DwmThumbnail.xaml.cs
@@ -1,57 +1,20 @@
 ﻿using StageManager.Native.Interop;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace StageManager
 {
-	/// <summary>
-	/// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-	///
-	/// Step 1a) Using this custom control in a XAML file that exists in the current project.
-	/// Add this XmlNamespace attribute to the root element of the markup file where it is 
-	/// to be used:
-	///
-	///     xmlns:MyNamespace="clr-namespace:StageManager"
-	///
-	///
-	/// Step 1b) Using this custom control in a XAML file that exists in a different project.
-	/// Add this XmlNamespace attribute to the root element of the markup file where it is 
-	/// to be used:
-	///
-	///     xmlns:MyNamespace="clr-namespace:StageManager;assembly=StageManager"
-	///
-	/// You will also need to add a project reference from the project where the XAML file lives
-	/// to this project and Rebuild to avoid compilation errors:
-	///
-	///     Right click on the target project in the Solution Explorer and
-	///     "Add Reference"->"Projects"->[Browse to and select this project]
-	///
-	///
-	/// Step 2)
-	/// Go ahead and use your control in the XAML file.
-	///
-	///     <MyNamespace:DwmThumbnail/>
-	///
-	/// </summary>
-	public class DwmThumbnail : Control
-	{
-		static DwmThumbnail()
-		{
-			DefaultStyleKeyProperty.OverrideMetadata(typeof(DwmThumbnail), new FrameworkPropertyMetadata(typeof(DwmThumbnail)));
-		}
+    /// <summary>
+    /// Interaction logic for DwmThumbnail.xaml
+    /// </summary>
+    public partial class DwmThumbnail : UserControl
+    {
+        public DwmThumbnail()
+        {
+            InitializeComponent();
+        }
 
 		private IntPtr _dwmThumbnail;
 
@@ -133,8 +96,8 @@ namespace StageManager
 				{
 					top = (int)(previewBounds.Top * dpi.Y),
 					left = (int)(previewBounds.Left * dpi.X),
-					bottom = (int)(previewBounds.Bottom * dpi.Y),
-					right = (int)(previewBounds.Right * dpi.X)
+					bottom = (int)((previewBounds.Bottom - Margin.Top - Margin.Bottom) * dpi.Y) + 1,
+					right = (int)((previewBounds.Right - Margin.Left - Margin.Right) * dpi.X) + 1
 				},
 				fSourceClientAreaOnly = true
 			};
