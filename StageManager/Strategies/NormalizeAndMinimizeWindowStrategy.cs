8f9baee5258e287742a12bdbe2e942d9564ef13b
awaescher <awaescher@gmx.de>
2023-12-03 12:36:56 +0100

    Improve window strategy handling

---
+++ StageManager/Strategies/NormalizeAndMinimizeWindowStrategy.cs
@@ -0,0 +1,18 @@
+﻿using StageManager.Native.PInvoke;
+using StageManager.Native.Window;
+
+namespace StageManager.Strategies
+{
+    internal class NormalizeAndMinimizeWindowStrategy : IWindowStrategy
+	{
+		public void Show(IWindow window)
+		{
+			Win32.ShowWindow(window.Handle, Win32.SW.SW_SHOWNOACTIVATE);
+		}
+
+		public void Hide(IWindow window)
+		{
+			Win32.ShowWindow(window.Handle, Win32.SW.SW_MINIMIZE);
+		}
+	}
+}
---
+++ StageManager/Strategies/ScreenOffsetWindowStrategy.cs
@@ -0,0 +1,17 @@
+﻿using StageManager.Native.Window;
+
+namespace StageManager.Strategies
+{
+	internal class ScreenOffsetWindowStrategy : IWindowStrategy
+	{
+		public void Show(IWindow window)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Hide(IWindow window)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
